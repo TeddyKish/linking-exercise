732345f02d207d16e419906887b64a0e1e5f0351
Teddy Kishinevsky <teddy@ted.com>
2022-09-22 23:51:07 +0300

    Added multiple definitions scenario

---
+++ 2/ext.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "helper.h"
+
+void func()
+{
+	printf("from ext, c = %d", c);
+}
---
+++ 2/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "helper.h"
+
+int main(int argc, char const *argv[])
+{
+	printf("from main, c = %d", c);
+	return 0;
+}
